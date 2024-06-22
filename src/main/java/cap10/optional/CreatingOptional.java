b67b896ab392ae892e69d27c0b8766784931b347
Matheus Rodrigues Bispo <matrodrigues1576@gmail.com>
2024-06-22 13:01:47 -0300

    add examples for optional

---
+++ src/main/java/cap10/optional/CreatingOptional.java
@@ -0,0 +1,13 @@
+package cap10.optional;
+
+import java.util.Optional;
+
+public class CreatingOptional {
+
+    /**
+     * Método estático que calcula uma média simples
+     * @param scores - valores das notas
+     * @return - retorna um Optional vazio se os valores do parametro forem nulos ou retorna o valor (DOUBLE) da média simples das notas em um Optional
+     */
+    //Criar método aqui
+}
